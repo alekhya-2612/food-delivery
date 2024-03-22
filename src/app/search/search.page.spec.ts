5d1506cc2320f6400050fb908e3dfdcc01fecce5
hajimalanbaba <Hajibaba@snvsoftwaresolutions.onmicrosoft.com>
2024-03-22 18:02:09 +0530

    food

--- src/app/search/search.page.spec.ts
+++ src/app/search/search.page.spec.ts
@@ -15,3 +15,7 @@ describe('SearchPage', () => {
     expect(component).toBeTruthy();
   });
 });
+function async(arg0: () => void): jasmine.ImplementationCallback {
+  throw new Error('Function not implemented.');
+}
+
